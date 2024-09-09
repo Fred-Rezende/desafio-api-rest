9fc06604bbe27b2d339d6465ec4d1a86c2ec92a2
Fred-Rezende <raizen.fred@gmail.com>
2024-09-09 17:05:59 -0300

    Implementação da camada de domínio

---
+++ src/main/java/dio/desafio_api_rest/domain/model/Feature.java
@@ -0,0 +1,9 @@
+package dio.desafio_api_rest.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+
+
+}
---
+++ src/main/java/dio/desafio_api_rest/domain/model/News.java
@@ -0,0 +1,9 @@
+package dio.desafio_api_rest.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+
+
+}
