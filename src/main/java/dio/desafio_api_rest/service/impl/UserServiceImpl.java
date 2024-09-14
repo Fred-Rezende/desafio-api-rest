066769765842cbdf83f4b4a988f123abba1ad04b
Fred-Rezende <raizen.fred@gmail.com>
2024-09-14 16:03:44 -0300

    Primeira tentativa de publicação no Railway

---
+++ src/main/java/dio/desafio_api_rest/service/impl/UserServiceImpl.java
@@ -0,0 +1,35 @@
+package dio.desafio_api_rest.service.impl;
+
+import dio.desafio_api_rest.domain.model.User;
+import dio.desafio_api_rest.domain.repository.UserRepository;
+import dio.desafio_api_rest.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private  final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User userToCreate) {
+        /*if(userToCreate.getId() != null && userRepository.existsById(userToCreate.getId())){
+            throw new IllegalArgumentException("This User ID already exists.");
+        }*/
+
+        if(userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())){
+            throw new IllegalArgumentException("This Account number already exists.");
+        }
+        return userRepository.save(userToCreate);
+    }
+}
